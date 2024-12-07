6aae6bcff121a86e67c0818713f303eca1242184
Nastja Demchyk <NastjaDemchyk@gmail.com>
2024-12-07 10:15:48 +0100

    completed 3 steps

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,37 @@
-import { Heading } from 'components';
+import { Header } from 'components';
+import Home from 'pages/Home';
+import Rates from 'pages/Rates';
+import { useEffect } from 'react';
+import { Routes, Route, Navigate } from 'react-router-dom';
+import { getUserInfo } from './service/opencagedataApi';
 
 export const App = () => {
-  return <Heading title="Just do it!" />;
+  useEffect(() => {
+    var options = {
+      enableHighAccuracy: true,
+      timeout: 5000,
+      maximumAge: 0,
+    };
+
+    function success(pos) {
+      getUserInfo(pos.coords);
+    }
+
+    function error(err) {
+      console.warn(`ERROR(${err.code}): ${err.message}`);
+    }
+
+    navigator.geolocation.getCurrentPosition(success, error, options);
+  }, []);
+
+  return (
+    <>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/rates" element={<Rates />} />
+        <Route path="*" element={<Navigate to="/" />} />
+      </Routes>
+    </>
+  );
 };
